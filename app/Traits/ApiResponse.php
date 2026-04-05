1ee8dc7b6d750fd124dfd81a3e5218efce57a8fc
andre.av <andre.veras.3@globalhitss.com.br>
2026-04-05 12:02:25 -0300

    feat(api): standardizing API response

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+    protected function success($data = [], $message = 'Sucesso', $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function error($message = 'Erro', $errors = [], $code = 400)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors
+        ], $code);
+    }
+}
